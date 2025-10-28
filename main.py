eda1a4c51d1453d6d04c3b49baff0b015b34e180
chldPwls <inyygo@gmail.com>
2025-10-28 14:43:29 +0900

    하트모양 출력되는 코드
    하트모양 출력되는 코드

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("하트")
+for y in range(15, -15, -1):
+    row = ''
+    for x in range(-30, 30):
+        if ((x * 0.05)**2 + (y * 0.1)**2 - 1)**3 - (x * 0.05)**2 * (y * 0.1)**3 <= 0:
+            row += '*'
+        else:
+            row += ' '
+    print(row)
